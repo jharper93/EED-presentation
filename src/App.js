e7abc07104e69381263f59ff450c221cee00354c
James Harper <jamh@dhigroup.com>
2021-02-08 13:08:50 +0100

    NA

--- src/App.js
+++ src/App.js
@@ -1,102 +1,94 @@
-import React from 'react'
-import './App.css';
-import styled from 'styled-components'
-import {BarGraph} from './components/barChart'
-import axios from  'axios'
+import React from "react";
+import "./App.css";
+import styled, { css } from "styled-components";
+import { BarGraph } from "./components/barChart";
+import axios from "axios";
 import firebase from "firebase/app";
 import "firebase/database";
 import {
   FirebaseDatabaseProvider,
-  FirebaseDatabaseNode
+  FirebaseDatabaseNode,
 } from "@react-firebase/database";
-
 
 const config = {
   apiKey: "AIzaSyDUY89t3eTe709M0hm3kUKsDaUsKxovMsQ",
   authDomain: "dhi-water-level-sensor.firebaseapp.com",
-  databaseURL: "https://dhi-water-level-sensor-default-rtdb.europe-west1.firebasedatabase.app",
+  databaseURL:
+    "https://dhi-water-level-sensor-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "dhi-water-level-sensor",
   storageBucket: "dhi-water-level-sensor.appspot.com",
   messagingSenderId: "801580174142",
   appId: "1:801580174142:web:415306920273bdc0583a6f",
-  measurementId: "G-Y95C4WH5HY"
+  measurementId: "G-Y95C4WH5HY",
 };
 
 const App = () => {
+  const [initialLevel, setInitialLevel] = React.useState(0);
+  const [currentLevel, setCurrentLevel] = React.useState(0);
 
-  const [level, setLevel] = React.useState(0)
+  React.useEffect(() => {
+    setCurrentLevel(initialLevel);
+  }, [initialLevel]);
 
-  const fetchData = async () => {
-
-
-    try { 
-      const {data} = await axios.get("https://cors-anywhere.herokuapp.com/https://csrng.net/csrng/csrng.php?min=5&max=95")
-
-     
-      setLevel(data[0].random)
-
-    } catch(err){
-      setLevel(50)
-    }
-
-        
-  }
+  const volume = (initialLevel = 0, currentLevel = 0) => {
+    return 0.1 * 0.15 * (currentLevel - initialLevel);
+  };
 
   return (
-    <FirebaseDatabaseProvider firebase={firebase} {...config}>
-      <FirebaseDatabaseNode
-            path="range/"
-            limitToFirst={level}
-         
-          >
-
-
-
-
-{(data = 0) => {
+    <div className="App">
+      <header className="App-header">
+        <FirebaseDatabaseProvider firebase={firebase} {...config}>
+          <FirebaseDatabaseNode path="range/" limitToFirst={0}>
+            {(data = 0) => {
+              setCurrentLevel(data.value?.value ?? 0);
 
               return (
-                <div className="App">
-                <header className="App-header">
-                  <$Container >
+                <>
+                  <$Container>
                     <$Header>Water level tester</$Header>
-                      <$BarGraphContainer>
-                          <BarGraph level={data.value.value}/>
-                      </$BarGraphContainer>
+                    <$BarGraphContainer>
+                      <BarGraph level={data.value?.value} />
+                    </$BarGraphContainer>
+                    <$Header>current volume: {volume(currentLevel)}</$Header>
+                    <$Header>Initial level: {initialLevel}</$Header>
+                    <$Header>Current level: {currentLevel}</$Header>
+                    <button onClick={() => setInitialLevel(currentLevel)}>
+                      Set initial level
+                    </button>
+                    <button onClick={() => volume(0, 5)}>
+                      Caclculate volume difference
+                    </button>
                   </$Container>
-                  <button onClick={fetchData}>
-                    click
-                  </button>
-                </header>
-              </div>)
+                </>
+              );
             }}
-
-  
-    </FirebaseDatabaseNode>
-    </FirebaseDatabaseProvider>
+          </FirebaseDatabaseNode>
+        </FirebaseDatabaseProvider>
+      </header>
+    </div>
   );
-}
+};
 
 const $Container = styled.div`
-display: flex;
-flex-direction: column;
-align-items: center;
-padding: 50px;
-width: 70%;
-min-height: 700px; 
-color:  #282c34;
-background-color: white;
-border-radius: 5px;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 50px;
+  width: 70%;
+  min-height: 700px;
+  color: #282c34;
+  background-color: white;
+  border-radius: 5px;
+`;
 
 const $Header = styled.div`
-font-size: '24px';
-font-weight: bold;
-`
+  font-size: "24px";
+  font-weight: bold;
+`;
 const $BarGraphContainer = styled.div`
-width: 80%;
-height: 200px;
-margin: 50px 0 50px -50px;
-`
+  width: 80%;
+  height: 200px;
+  margin: 50px 0 50px -50px;
+`;
 
 export default App;
--- src/components/barChart.js
+++ src/components/barChart.js
@@ -1,35 +1,44 @@
-import React from 'react'
+import React from "react";
 import {
-    ComposedChart, Bar, ResponsiveContainer, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
-  } from 'recharts';
+  ComposedChart,
+  Bar,
+  ResponsiveContainer,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
 
-export const BarGraph = ({level}) => {
+export const BarGraph = ({ level = 0 }) => {
+  const data = [
+    {
+      name: "Water level",
+      level: level,
+    },
+  ];
 
-    const data = [
-        {
-          name: 'Water level',  level: level 
-        },
-      ];
-      
-
-    return (
-        <ResponsiveContainer>
-          <ComposedChart
+  return (
+    <ResponsiveContainer>
+      <ComposedChart
         layout="vertical"
         width={500}
         height={100}
         data={data}
         margin={{
-          top: 20, right: 20, bottom: 20, left: 20,
+          top: 20,
+          right: 20,
+          bottom: 20,
+          left: 20,
         }}
       >
         <CartesianGrid stroke="#f5f5f5" />
-        <XAxis type="number" domain={[0, 10]}/>
+        <XAxis type="number" domain={[0, 45]} />
         <YAxis dataKey="name" type="category" />
         <Tooltip />
         <Legend />
         <Bar dataKey="level" barSize={40} fill="#413ea0" />
       </ComposedChart>
-      </ResponsiveContainer>
-    )
-}
+    </ResponsiveContainer>
+  );
+};
